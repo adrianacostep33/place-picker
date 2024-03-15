f52d7b9dd5a5f18c93eb360688374df61be1eccf
adrianacostep33 <adriana.coste@principal33.com>
2024-03-15 21:27:10 +0200

    Add progress bar component

---
+++ 01-starting-project/src/components/ProgressBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const ProgressBar = ({ timer }) => {
+  const [remainingTime, setRemainingTime] = useState(timer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+      s;
+    };
+  }, []);
+  return <progress value={remainingTime} max={timer} />;
+};
+
+export default ProgressBar;
